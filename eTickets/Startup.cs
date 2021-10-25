1c16b542c904270d8b0d933f2df98bda19e2a58e
Vice-Inc <42152600+Vice-Inc@users.noreply.github.com>
2021-10-25 16:39:15 +0300

    Add Initial Migration

--- eTickets/Startup.cs
+++ eTickets/Startup.cs
@@ -2,6 +2,7 @@ using eTickets.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -21,9 +22,12 @@ namespace eTickets
 
         public IConfiguration Configuration { get; }
 
+        //Add-Migration Initial
+        //Update-Database
+
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<AppDbContext>();
+            services.AddDbContext<AppDbContext>(options => options.UseNpgsql(Configuration.GetConnectionString("DefaultConnectionString")));
             services.AddControllersWithViews();
         }
 
