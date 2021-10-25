7d52340bb59eef8d6c204a5083923f9028e9f108
Vice-Inc <42152600+Vice-Inc@users.noreply.github.com>
2021-10-25 11:46:21 +0300

    Add models and AppDbContext

--- eTickets/Startup.cs
+++ eTickets/Startup.cs
@@ -1,3 +1,4 @@
+using eTickets.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -20,13 +21,12 @@ namespace eTickets
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>();
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
