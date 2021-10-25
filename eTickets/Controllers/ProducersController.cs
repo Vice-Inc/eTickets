0192408b697707ccacc5deb6cee0b209bf96275a
Vice-Inc <42152600+Vice-Inc@users.noreply.github.com>
2021-10-25 21:15:03 +0300

    Producer`s View

--- eTickets/Controllers/ProducersController.cs
+++ eTickets/Controllers/ProducersController.cs
@@ -21,7 +21,7 @@ namespace eTickets.Controllers
         {
             var data = await context.Producers.ToListAsync();
 
-            return View();
+            return View(data);
         }
     }
 }
