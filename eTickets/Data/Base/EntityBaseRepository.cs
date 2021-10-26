703e85e45aa82566eddbd50a4e40dd0434e58872
Vice-Inc <42152600+Vice-Inc@users.noreply.github.com>
2021-10-26 12:48:53 +0300

    Create EntityBaseRepository

---
+++ eTickets/Data/Base/EntityBaseRepository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.Base
+{
+    public class EntityBaseRepository<T> : IEntityBaseRepository<T> where T : class, IEntityBase, new()
+    {
+        private readonly AppDbContext context;
+
+        public EntityBaseRepository(AppDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await context.Set<T>().FirstOrDefaultAsync(a => a.id == id);
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await context.Set<T>().AddAsync(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(int id, T newEntity)
+        {
+            EntityEntry entityEntry = context.Entry<T>(newEntity);
+            entityEntry.State = EntityState.Modified;
+            //context.Update(newEntity);
+            //await context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            T entity = await GetByIdAsync(id);
+            EntityEntry entityEntry = context.Entry<T>(entity);
+            entityEntry.State = EntityState.Deleted;
+            //T entity = await GetByIdAsync(id);
+            //context.Set<T>().Remove(entity);
+            //await context.SaveChangesAsync();
+        }
+    }
+}
