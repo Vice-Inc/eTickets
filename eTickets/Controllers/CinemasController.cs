10d3054f5f776478352df38bd286163c1c174a51
Vice-Inc <42152600+Vice-Inc@users.noreply.github.com>
2021-10-26 14:59:57 +0300

    Add CRUD for producers and cinemas

--- eTickets/Controllers/CinemasController.cs
+++ eTickets/Controllers/CinemasController.cs
@@ -1,5 +1,6 @@
 ﻿using eTickets.Data;
 using eTickets.Data.Interfaces;
+using eTickets.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -20,9 +21,65 @@ namespace eTickets.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var data = await cinemasService.GetAllAsync();
+            var allCinemas = await cinemasService.GetAllAsync();
+            return View(allCinemas);
+        }
 
-            return View(data);
+
+        //Get: Cinemas/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Logo,Name,Description")] Cinema cinema)
+        {
+            if (!ModelState.IsValid) return View(cinema);
+            await cinemasService.AddAsync(cinema);
+            return RedirectToAction(nameof(Index));
+        }
+
+        //Get: Cinemas/Details/1
+        public async Task<IActionResult> Details(int id)
+        {
+            var cinemaDetails = await cinemasService.GetByIdAsync(id);
+            if (cinemaDetails == null) return View("NotFound");
+            return View(cinemaDetails);
+        }
+
+        //Get: Cinemas/Edit/1
+        public async Task<IActionResult> Edit(int id)
+        {
+            var cinemaDetails = await cinemasService.GetByIdAsync(id);
+            if (cinemaDetails == null) return View("NotFound");
+            return View(cinemaDetails);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("id,Logo,Name,Description")] Cinema cinema)
+        {
+            if (!ModelState.IsValid) return View(cinema);
+            await cinemasService.UpdateAsync(id, cinema);
+            return RedirectToAction(nameof(Index));
+        }
+
+        //Get: Cinemas/Delete/1
+        public async Task<IActionResult> Delete(int id)
+        {
+            var cinemaDetails = await cinemasService.GetByIdAsync(id);
+            if (cinemaDetails == null) return View("NotFound");
+            return View(cinemaDetails);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirm(int id)
+        {
+            var cinemaDetails = await cinemasService.GetByIdAsync(id);
+            if (cinemaDetails == null) return View("NotFound");
+
+            await cinemasService.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- eTickets/Controllers/ProducersController.cs
+++ eTickets/Controllers/ProducersController.cs
@@ -1,5 +1,6 @@
 ﻿using eTickets.Data;
 using eTickets.Data.Interfaces;
+using eTickets.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -20,9 +21,70 @@ namespace eTickets.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var data = await producersService.GetAllAsync();
+            var allProducers = await producersService.GetAllAsync();
+            return View(allProducers);
+        }
 
-            return View(data);
+        //GET: producers/details/1
+        public async Task<IActionResult> Details(int id)
+        {
+            var producerDetails = await producersService.GetByIdAsync(id);
+            if (producerDetails == null) return View("NotFound");
+            return View(producerDetails);
+        }
+
+        //GET: producers/create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("ProfilePictureURL,FullName,Bio")] Producer producer)
+        {
+            if (!ModelState.IsValid) return View(producer);
+
+            await producersService.AddAsync(producer);
+            return RedirectToAction(nameof(Index));
+        }
+
+        //GET: producers/edit/1
+        public async Task<IActionResult> Edit(int id)
+        {
+            var producerDetails = await producersService.GetByIdAsync(id);
+            if (producerDetails == null) return View("NotFound");
+            return View(producerDetails);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("id,ProfilePictureURL,FullName,Bio")] Producer producer)
+        {
+            if (!ModelState.IsValid) return View(producer);
+
+            if (id == producer.id)
+            {
+                await producersService.UpdateAsync(id, producer);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(producer);
+        }
+
+        //GET: producers/delete/1
+        public async Task<IActionResult> Delete(int id)
+        {
+            var producerDetails = await producersService.GetByIdAsync(id);
+            if (producerDetails == null) return View("NotFound");
+            return View(producerDetails);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var producerDetails = await producersService.GetByIdAsync(id);
+            if (producerDetails == null) return View("NotFound");
+
+            await producersService.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
